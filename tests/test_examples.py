b12e051159960268429da8985d54118e06d4401f
Vitaliy Mysak <vmysak@bccfe.ca>
2024-08-07 14:36:02 -0700

    Add write example test

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,6 +1,8 @@
 
 from pathlib import Path
 import pytest
+import io
+import csv
 
 import multicsv
 
@@ -39,3 +41,20 @@ def test_read_csv(example_file: Path) -> None:
 
         assert section1 == [['d', 'e', 'f'],
                             ['4', '5', '6']]
+
+
+def test_write_csv(example_file):
+    with multicsv.open(example_file, mode='w+') as csv_file:
+        # Write the CSV content to the file
+        csv_file['section1'] = io.StringIO("header1,header2,header3\nvalue1,value2,value3\n")
+        csv_file['section2'] = io.StringIO("header4,header5,header6\nvalue4,value5,value6\n")
+
+        # Read a section using the csv module
+        section1 = csv_file['section1']
+        csv_reader = csv.reader(section1)
+        assert list(csv_reader) == [['header1', 'header2', 'header3'],
+                                    ['value1', 'value2', 'value3']]
+
+        # Get all sections:
+        all_sections = list(csv_file)
+        assert ['section1', 'section2'] == all_sections
